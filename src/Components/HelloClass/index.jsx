594741eedb1bb1da6fde5b4bd62dfe2953e9e800
Serg0501 <gavrilenko87sd@gmail.com>
2022-07-14 08:59:55 +0300

    test

---
+++ src/Components/HelloClass/index.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+export default class HelloClass extends Component {
+  constructor(props){
+    super(props);
+  }
+  render() {
+    const {name, surname} = this.props;
+    return (
+        <>
+            <p>Hello, {name} {surname}!</p>
+        </>
+    )
+  }
+}
